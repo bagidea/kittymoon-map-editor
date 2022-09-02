7cc2a25ce752f40353335958db43b10cb2aef039
bagidea <thanawat@nexhe.com>
2022-09-03 00:18:01 +0700

    create input function

--- src/map_editor/map_editor.tsx
+++ src/map_editor/map_editor.tsx
@@ -3,6 +3,8 @@ import CoreEngine from "../core3d/core_engine";
 class MapEditor extends CoreEngine {
     private frame: HTMLDivElement
 
+    private keyPress: Map<string, boolean> = new Map<string, boolean>()
+
     constructor(canvas: HTMLCanvasElement, frame: HTMLDivElement) {
         super(canvas)
 
@@ -12,7 +14,54 @@ class MapEditor extends CoreEngine {
     create() {
     }
 
+    keydown = (e: KeyboardEvent) => {
+        switch(e.key) {
+            case "ArrowUp":
+                this.keyPress.set("ArrowUp", true)
+                break
+            case "ArrowDown":
+                this.keyPress.set("ArrowDown", true)
+                break
+            case "ArrowLeft":
+                this.keyPress.set("ArrowLeft", true)
+                break
+            case "ArrowRight":
+                this.keyPress.set("ArrowRight", true)
+                break
+            case "Shift":
+                this.keyPress.set("Shift", true)
+                break
+        }
+    }
+
+    keyup = (e: KeyboardEvent) => {
+        switch(e.key) {
+            case "ArrowUp":
+                this.keyPress.set("ArrowUp", false)
+                break
+            case "ArrowDown":
+                this.keyPress.set("ArrowDown", false)
+                break
+            case "ArrowLeft":
+                this.keyPress.set("ArrowLeft", false)
+                break
+            case "ArrowRight":
+                this.keyPress.set("ArrowRight", false)
+                break
+            case "Shift":
+                this.keyPress.set("Shift", false)
+                break
+        }
+    }
+
     input() {
+        this.keyPress.set("ArrowUp", false)
+        this.keyPress.set("ArrowDown", false)
+        this.keyPress.set("ArrowLeft", false)
+        this.keyPress.set("ArrowRight", false)
+
+        window.addEventListener('keydown', this.keydown)
+        window.addEventListener('keyup', this.keyup)
     }
 }
 
