2e484b73ae9e100e0223c4849975c77b37a977c9
bagidea <thanawat@nexhe.com>
2022-09-19 06:07:47 +0700

    add logo

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,10 +1,15 @@
 import { ChakraProvider } from "@chakra-ui/react"
+import Head from "next/head"
 import theme from "../src/theme"
 
 const App = ({ Component, pageProps }) => (
     <ChakraProvider
         theme={ theme }
     >
+        <Head>
+            <link rel="icon" type="image/png" href="/icon.png" />
+        </Head>
+
         <Component
             { ...pageProps }
         />
